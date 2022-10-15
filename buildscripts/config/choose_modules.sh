cd9e7a92c3eb815dd7f8d90b6123425cf0135fd6
Yonggang Yu <yonggangyu@ucar.edu>
2022-10-14 19:03:57 -0600

    - set to N: cmake, gitlfs, lapack, eigen as in mac guide - add myenv.mac

--- buildscripts/config/choose_modules.sh
+++ buildscripts/config/choose_modules.sh
@@ -4,15 +4,15 @@
 # http://www.apache.org/licenses/LICENSE-2.0.
 
 # Minimal JEDI Stack
-export      STACK_BUILD_CMAKE=Y
+export      STACK_BUILD_CMAKE=N
 export     STACK_BUILD_GITLFS=N
 export       STACK_BUILD_SZIP=Y
 export    STACK_BUILD_UDUNITS=Y
 export       STACK_BUILD_ZLIB=Y
-export     STACK_BUILD_LAPACK=Y
+export     STACK_BUILD_LAPACK=N
 export STACK_BUILD_BOOST_HDRS=Y
 export       STACK_BUILD_BUFR=Y
-export     STACK_BUILD_EIGEN3=Y
+export     STACK_BUILD_EIGEN3=N
 export       STACK_BUILD_HDF5=Y
 export    STACK_BUILD_PNETCDF=Y
 export     STACK_BUILD_NETCDF=Y
@@ -26,9 +26,9 @@ export   STACK_BUILD_PYBIND11=Y
 
 # Optional Additions
 export           STACK_BUILD_ODC=N
-export           STACK_BUILD_PIO=N
+export           STACK_BUILD_PIO=Y
 export          STACK_BUILD_GPTL=N
-export           STACK_BUILD_NCO=N
+export           STACK_BUILD_NCO=Y
 export        STACK_BUILD_PYJEDI=N
 export      STACK_BUILD_NCEPLIBS=N
 export          STACK_BUILD_JPEG=N
